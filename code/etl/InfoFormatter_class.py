fb63008107f57c2621f110d201c514235f172884
fembrioni <fembrioni@gmail.com>
2023-07-04 14:50:39 -0300

    Info formatter

---
+++ code/etl/InfoFormatter_class.py
@@ -0,0 +1,31 @@
+# Este archivo contiene la clase abstracta InfoFormatter
+
+# Imports
+
+import json
+import pandas as pd
+from abc import ABC, abstractmethod
+from enum import Enum
+
+class InfoFormatter(ABC):
+    '''
+    Un InfoFormatter es aquel modulo que toma la información en formato Json y produce una salida
+    en formato estandarizado con la información extraída.
+    Esta clase es una clase abstracta que plantea los mensajes que sus instancias deben implementar.
+    '''
+
+    @abstractmethod
+    def formatInfo(self, country: str, jsonData: json) -> (str, pd.DataFrame):
+        '''
+        Este metodo permite formatear información de un país dado utilizando jsonData como origen de los datos.
+        country identifica el país al cual pertenecen los datos.
+
+        Ejemplo:
+
+        formatInfo(country='US',
+                    jsonData=objetoJson)
+
+        Ejemplo salida:
+
+        Tupla de(String de error, objeto DataFrame conteniendo la info formateada según la estandarización)
+        '''

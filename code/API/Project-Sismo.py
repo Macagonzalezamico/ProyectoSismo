8697231f1dbbcd3cb8ba16e63b459cc12d98a9d7
MichelVillot <107226318+MichelVillot@users.noreply.github.com>
2023-07-17 16:07:21 -0400

    Update Project-Sismo.py

--- code/API/Project-Sismo.py
+++ code/API/Project-Sismo.py
@@ -3,8 +3,8 @@
 from PIL import Image
 import time
 #from streamlit_extras.switch_page_button import switch_page
-from streamlit_extras.metric_cards import style_metric_cards
-from streamlit_option_menu import option_menu
+#from streamlit_extras.metric_cards import style_metric_cards
+#from streamlit_option_menu import option_menu
 #import plost
 #import plotly.express as px
 #import seaborn as sns
